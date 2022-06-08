d401f91c8568b34dc31d49a0a8b9e73a76b35c8e
eunryong <studyeunryong@gmail.com>
2022-06-08 20:33:05 +0900

    숙제1

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,8 +4,7 @@ import { useState } from 'react';
 
 function App() {
 
-  let post = '진주 냉면 맛집';
-  let [글제목, b] =useState('남자 셔츠 추천')
+  let [글제목, b] =useState(['남자 셔츠 추천', '진주 냉면 맛집', 'React 독학'])
   
   return (
     <div className="App">
@@ -13,7 +12,15 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>{글제목[0]}</h4>
+        <p>6월 8일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[1]}</h4>
+        <p>6월 8일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>6월 8일 발행</p>
       </div>
     </div>
