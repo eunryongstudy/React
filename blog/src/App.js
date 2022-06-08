41175449684d4e0ec9f7b2d0cd50c2ea5ad4ae4c
eunryong <studyeunryong@gmail.com>
2022-06-08 20:28:03 +0900

    state, 변수 이용

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '진주 냉면 맛집';
+  let [글제목, b] =useState('남자 셔츠 추천')
   
-
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>Blog</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{글제목}</h4>
+        <p>6월 8일 발행</p>
+      </div>
     </div>
   );
 }
