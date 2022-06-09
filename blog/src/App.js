a76100f9cedab1261bdd383d1ea1de78040fd96c
eunryong <studyeunryong@gmail.com>
2022-06-09 20:26:29 +0900

    component modal

--- blog/src/App.js
+++ blog/src/App.js
@@ -39,8 +39,20 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>6월 8일 발행</p>
       </div>
+      
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
