02ddb389f487b2779f91cd64b53ca3a84afe0917
eunryong <studyeunryong@gmail.com>
2022-06-14 21:40:44 +0900

    routes

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar }  from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
+import { Routes, Route, Link} from 'react-router-dom';
+import Detail from './routes/detail.js';
 
 function App() {
 
@@ -10,6 +12,14 @@ function App() {
 
   return (
     <div className="App">
+
+
+      
+
+
+
+
+
        <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">은룡shop</Navbar.Brand>
@@ -20,13 +30,22 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className='main-bg'></div>
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
 
-      <div className="container">
-        <div className="row">
-        <상품 shoes = {shoes}/>
-      </div>
-      </div> 
+      <Routes>
+        <Route path="/" element={<>
+          <div className='main-bg'></div>
+          <div className="container">
+            <div className="row">
+            <상품 shoes = {shoes}/>
+          </div>
+          </div> 
+        </>} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+
+      
 
       
     </div>
