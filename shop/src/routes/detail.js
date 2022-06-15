d5733a0c0dc26ac3a1847b717b66ddac87aa09db
eunryong <studyeunryong@gmail.com>
2022-06-15 21:39:50 +0900

    useEffect 숙제3

--- shop/src/routes/detail.js
+++ shop/src/routes/detail.js
@@ -1,36 +1,46 @@
+import React, { useEffect, useState } from "react";
 import {useParams} from "react-router-dom";
-import styled from "styled-components";
 
-let YellowBtn = styled.button`
-    background : yellow;
-    color : black;
-    padding : 10px`
-
-let Box = styled.div`
-    background : grey;
-    padding : 20px;
-`
 
 function Detail(props){
+
+    
+
+    let[count, setCount] = useState(0);
+    let[alert, setAlert] = useState(true);
     let {id} = useParams();
     let 찾은상품 = props.shoes.find(function(x){
         return x.id == id
       });
 
+      useEffect(()=>{
+
+        setTimeout(()=>{
+            setAlert(false)
+        }, 2000,[])
+    })
+
     return (
         <>
         <div className="container">
-        <div className="row">
-            <div className="col-md-6">
-            <img src={"https://codingapple1.github.io/shop/shoes"+(찾은상품.id)+".jpg"} width="100%" />
+            {alert == true ? 
+            <div className="alert alert-warning">
+            2초이내 구매시 할인
+            </div> 
+            : null}
+            
+            <button onClick ={()=>{ setCount(count+1)}}>버튼</button>
+            <div className="row">
+                <div className="col-md-6">
+                <img src={"https://codingapple1.github.io/shop/shoes"+(찾은상품.id)+".jpg"} width="100%" />
+                </div>
+                <div className="col-md-6">
+                <h4 className="pt-5">{찾은상품.title}</h4>
+                <p>{찾은상품.content}</p>
+                <p>가격 : {찾은상품.price}</p>
+                <button className="btn btn-danger">주문하기</button> 
+                </div>
             </div>
-            <div className="col-md-6">
-            <h4 className="pt-5">{찾은상품.title}</h4>
-            <p>{찾은상품.content}</p>
-            <p>가격 : {찾은상품.price}</p>
-            <button className="btn btn-danger">주문하기</button> 
-            </div>
-        </div>
         </div>
         </>
     )
